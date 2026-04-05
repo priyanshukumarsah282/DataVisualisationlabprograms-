9f47f71fe7225986c4871396060c604e9478e5e5
Priyanshu Kumar sah <psah44949@gmail.com>
2026-04-05 14:19:23 +0530

    Delete data-visualisation-lab-main directory

--- data-visualisation-lab-main/04_dynamic_histogram.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-
-data = list(map(int,input("Enter data values:").split()))
-plt.hist(data, bins=5)
-plt.xlabel("Data values")
-plt.ylabel("Frequency")
-plt.title("Simple Histogram")
-plt.show()
--- data-visualisation-lab-main/07_static_pie_chart.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-labels = ['A', 'B', 'C', 'D']
-values = [10, 20, 15, 25]
-
-plt.pie(values, labels=labels, autopct='%1.1f%%')
-plt.title("Static Pie Chart")
-
-plt.show()
